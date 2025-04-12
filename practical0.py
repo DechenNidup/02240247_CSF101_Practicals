e5a8e1f21194b46960cb60d281e29ee395ae4e61
DechenNidup <02240247.cst@rub.edu.bt>
2025-04-12 09:13:30 +0530

    my second commit

---
+++ practical0.py
@@ -0,0 +1,4 @@
+studentName = "Dechen Nidup"
+studentNumber = "02240247"
+
+print("My name is " + studentName + " and my student number is " + studentNumber)
--- practical1.py
+++ practical1.py
@@ -1,4 +1,22 @@
-studentName = "Dechen Nidup"
-studentNumber = "02240247"
+my_friend_array=["Dog", 2, 2.1, True, None]
+myfirstarraylength= len(my_friend_array)
+my_friend_array.append("cute")
+mysecondarraylength=len(my_friend_array)
+print(myfirstarraylength-mysecondarraylength)
 
-print("My name is " + studentName + " and my student number is " + studentNumber)
+
+family=["Dad", "Mom","Sis", "Bro"]
+arraylength=len(family)
+family.append("Me") 
+
+family=["Dad", "Mom","Sis", "Bro"]
+family.append("Me") 
+print(family)
+arraylength=len(family)
+for index in range(arraylength):
+    print(family[index])
+    
+index=0
+while index<arraylength:
+    print(family[index])
+    index=index + 1
